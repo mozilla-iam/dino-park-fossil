5c1c620401640993b7d615b07789c72765a2793f
Alberto del Barrio <adelbarrio@mozilla.com>
2020-06-03 14:05:49 +0200

    Keep only last 30 images; safe money

--- terraform/codebuild/main.tf
+++ terraform/codebuild/main.tf
@@ -162,3 +162,25 @@ resource "aws_ecr_repository_policy" "registrypolicy" {
 EOF
 }
 
+resource "aws_ecr_lifecycle_policy" "keep-30" {
+  repository = aws_ecr_repository.registry.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 30 images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 30
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
