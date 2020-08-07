efbb6753719cb226f8822ce506e8807ef1554378
Jens Hausdorf <mail@jens-hausdorf.de>
2020-08-07 10:52:24 +0200

    Address review comments

--- src/send/resize.rs
+++ src/send/resize.rs
@@ -4,7 +4,7 @@ use failure::Error;
 use image::imageops::FilterType;
 use image::DynamicImage;
 use image::GenericImageView;
-use log::info;
+use log::debug;
 
 pub struct Avatars {
     pub raw: Vec<u8>,
@@ -30,6 +30,9 @@ impl Avatars {
 
         // Copy the necessary data from the original image the image crate does not pick up manually
         let metadata_to_add = Avatars::maybe_extract_png_color_metadata(&buf)?;
+        if !metadata_to_add.is_empty() {
+            debug!("copied png color metadata to insert again after downsizing");
+        }
 
         Ok(Avatars {
             raw: buf,
@@ -43,42 +46,38 @@ impl Avatars {
     /// Returns png chunks that needs to be copied to keep color information
     /// related things intact (i.e. copy chunks that the image crate does not pick up)
     fn maybe_extract_png_color_metadata(buf: &[u8]) -> Result<Vec<u8>, Error> {
-        let mut metadata_to_add = Vec::new();
-
         let mut png_decoder = lodepng::Decoder::new();
         // required so the chunks are remembered (by default, it only remembers the mandatory chunks)
         png_decoder.remember_unknown_chunks(true);
         png_decoder.decode(&buf)?;
 
-        let mut added_chunks = Vec::new();
-
-        for chunk_to_copy in &["cHRM", "gAMA", "sRGB", "iCCP", "eXIf"] {
-            if let Some(data_chunk) = png_decoder.info_png().get(chunk_to_copy) {
-                // chunk_length = padding (2 bytes) chunk_len (4 bytes) + chunk type (4 bytes) + data_len + crc (8 bytes) = data_len + 18
-                metadata_to_add.reserve(data_chunk.len() + 18);
-
-                // write two bytes of padding
-                metadata_to_add.extend(&[0x00, 0x00]);
-                metadata_to_add
-                    .write_u16::<byteorder::BE>(data_chunk.len() as u16)
-                    .unwrap();
-                metadata_to_add.extend(&data_chunk.name());
-                metadata_to_add.extend(data_chunk.data());
-
-                metadata_to_add
-                    .write_u32::<byteorder::BE>(data_chunk.crc())
-                    .unwrap();
-
-                added_chunks.push(chunk_to_copy);
-            }
-        }
-
-        info!(
-            "Will manually transfer the following png metadata chunks into the downsized images: {:?}",
-            added_chunks
-        );
+        let metadata_to_add = ["cHRM", "gAMA", "sRGB", "iCCP", "eXIf"]
+            .iter()
+            .filter_map(|chunk_to_copy| png_decoder.info_png().get(chunk_to_copy))
+            .map(Avatars::metadata)
+            .flatten()
+            .collect();
 
         Ok(metadata_to_add)
+    }
+
+    fn metadata(data_chunk: lodepng::ChunkRef) -> Vec<u8> {
+        // chunk_length = padding (2 bytes) chunk_len (4 bytes) + chunk type (4 bytes) + data_len + crc (8 bytes) = data_len + 18
+        let mut metadata_to_add = Vec::with_capacity(data_chunk.len() + 18);
+
+        // write two bytes of padding
+        metadata_to_add.extend(&[0x00, 0x00]);
+        metadata_to_add
+            .write_u16::<byteorder::BE>(data_chunk.len() as u16)
+            .unwrap();
+        metadata_to_add.extend(&data_chunk.name());
+        metadata_to_add.extend(data_chunk.data());
+
+        metadata_to_add
+            .write_u32::<byteorder::BE>(data_chunk.crc())
+            .unwrap();
+
+        metadata_to_add
     }
 }
 
@@ -88,16 +87,12 @@ fn downsize(size: u32, img: &DynamicImage, metadata_to_add: &[u8]) -> Result<Vec
     down_sized.write_to(&mut buf, image::ImageOutputFormat::Png)?;
 
     if !metadata_to_add.is_empty() {
+        // 8-byte png file signature + IHDR chunk (length (4 bytes) + chunk type (4 bytes) + 13 data bytes + 4 bytes crc) = 33 bytes
         const IMAGE_START_OFFSET_TO_ADD_DATA: usize = 33;
 
-        // temporarily cut off the image bytes after the (fixed) header
+        // temporarily split off the image bytes after the (fixed) header
         // as most of the metadata we need to add must appear before any other tag
-        let image_data = &buf[IMAGE_START_OFFSET_TO_ADD_DATA..]
-            .iter()
-            .copied()
-            .collect::<Vec<u8>>();
-
-        buf.truncate(IMAGE_START_OFFSET_TO_ADD_DATA);
+        let image_data = buf.split_off(IMAGE_START_OFFSET_TO_ADD_DATA);
 
         buf.extend(metadata_to_add);
         buf.extend(image_data);
